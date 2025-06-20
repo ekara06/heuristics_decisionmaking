b8ec5808a759cbc1b7302e20e07ddc9272932d36
ekara06 <148986246+ekara06@users.noreply.github.com>
2025-06-20 12:05:29 +0200

    Update R_D_known.py

--- main_1/R_D_known.py
+++ main_1/R_D_known.py
@@ -3,19 +3,20 @@ import pandas as pd
 # Load the CSV
 df = pd.read_csv("comb_comparison.csv")
 
-#Get 50 rows where known_ranking == True
-known_ranking_df = df[df['known_ranking'] == True].head(50)
+# Get 50 rows where known_ranking == True
+#known1_ranking_df = df[(df['known_ranking'] == True)& (df['known_direction'] == True)].head(25)
+known2_ranking_df = df[(df['known_ranking'] == True)& (df['known_direction'] == False)].head(50)
 
-#Get 25 rows where known_direction == True
-known_direction_df = df[df['known_direction'] == True].head(25)
+# Save to CSV (concate)
+#known_ranking_df = pd.concat([known1_ranking_df, known2_ranking_df])
+known2_ranking_df.to_csv("known_ranking.csv", index=False)
 
-#Get 25 rows where known_direction == False
-known_direction_false_df = df[df['known_direction'] == False].head(25)
+# Get 50 rows where known_direction == True
+#known1_direction_df = df[(df['known_ranking'] == True)& (df['known_direction'] == True)].head(25)
+known2_direction_df = df[(df['known_ranking'] == False)& (df['known_direction'] == True)].head(50)
 
-#combine these 100 rows into a new dataframe
-known_df = pd.concat([known_ranking_df, known_direction_df, known_direction_false_df])
+# Combine these 50 rows into one dataframe for known_direction (concate)
+#known_direction_df = pd.concat([known1_direction_df, known2_direction_df])
 
-#save the new dataframe to a new csv file
-known_df.to_csv("known_df.csv", index=False)
-
-
+# Save to CSV
+known2_direction_df.to_csv("known_direction.csv", index=False)
