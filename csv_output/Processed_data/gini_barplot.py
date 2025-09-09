004f444bedf39ce165d5f595698833017a5caca3
ekara06 <148986246+ekara06@users.noreply.github.com>
2025-09-09 13:32:35 +0200

    Refactor gini_barplot.py for task-based processing

--- csv_output/Processed_data/gini_barplot.py
+++ csv_output/Processed_data/gini_barplot.py
@@ -3,21 +3,21 @@ import numpy as np
 from scipy.optimize import minimize
 import matplotlib.pyplot as plt
 
-files = ["/jatos_results_data_20250826/csv_output/Processed_data/ranking_data.csv", "/jatos_results_data_20250826/csv_output/Processed_data/direction_data.csv"]
+files = ["/Users/elifkara/Desktop/jatos_results_data_20250826/csv_output/Processed_data/ranking_data.csv", "/Users/elifkara/Desktop/jatos_results_data_20250826/csv_output/Processed_data/direction_data.csv"]
 all_ginis = []
 labels = []
 
 for file in files:
     df = pd.read_csv(file)
-    df["trial"] = df["optionA_trial_id"].astype(str) + "_" + df["optionB_trial_id"].astype(str)
+    df["task"] = df["optionA_task_id"].astype(str) + "_" + df["optionB_task_id"].astype(str)
 
     # Vectorized binary comparison
-    df['feature1_value_F_bin'] = (df['optionA_value1'] > df['optionA_value2']).astype(float)
-    df['feature2_value_F_bin'] = (df['optionB_value1'] > df['optionB_value2']).astype(float)
+    df['feature1_value_F_bin'] = (df['optionA_value1'] > df['optionB_value1']).astype(float)
+    df['feature2_value_F_bin'] = (df['optionA_value2'] > df['optionB_value2']).astype(float)
 
     # Assuming 'J_bin' should be 0 when 'F > J', and 1 otherwise:
-    df['feature1_value_J_bin'] = (df['optionA_value1'] <= df['optionA_value2']).astype(float)
-    df['feature2_value_J_bin'] = (df['optionB_value1'] <= df['optionB_value2']).astype(float)
+    df['feature1_value_J_bin'] = (df['optionA_value1'] <= df['optionB_value1']).astype(float)
+    df['feature2_value_J_bin'] = (df['optionA_value2'] <= df['optionB_value2']).astype(float)
 
     w1 = 1
     w2 = 1
@@ -25,13 +25,13 @@ for file in files:
 
     file_ginis = []
 
-    for trial in df['trial'].unique():
-        df_task = df[df['trial'] == trial]
+    for task in df['task'].unique():
+        df_task = df[df['task'] == task]
 
         def neg_loglikelihood(x):
             y_F = x[0]*df_task["feature1_value_F_bin"] + x[1]*df_task["feature2_value_F_bin"]
             y_J = x[0]*df_task["feature1_value_J_bin"] + x[1]*df_task["feature2_value_J_bin"]
-            y_difference = x[2] + (y_F - y_J)
+            y_difference = x[2] * (y_F - y_J)
 
             prob_F = 1 / (1 + np.exp(-y_difference))
             human_choices = np.where(df_task["button_pressed"] == "F", 1, 0)
